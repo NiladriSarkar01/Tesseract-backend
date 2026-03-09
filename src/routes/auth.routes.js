a57425338c2b2ed7d1d10b68ec7d9c280b6884b9
NiladriSarkar01 <niladrisarkar422@gmail.com>
2026-03-09 17:32:11 +0530

    super coders registration fixed.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,14 @@
-import express from 'express';
+import express from "express";
 
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { checkAuth, login, logout, signup } from '../controllers/auth.controller.js';
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { checkAuth, login, logout } from "../controllers/auth.controller.js";
 
 const authRoutes = express.Router();
 
-authRoutes.post('/signup', signup);
+authRoutes.post("/login", login);
 
-authRoutes.post('/login', login);
+authRoutes.post("/logout", logout);
 
-authRoutes.post('/logout', logout);
-
-authRoutes.get('/check',protectRoute, checkAuth);
+authRoutes.get("/check", protectRoute, checkAuth);
 
 export default authRoutes;
